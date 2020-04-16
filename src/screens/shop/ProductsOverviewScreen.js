1030be5a59e141774b45c7e1914a1349410feec0
Serdar Mustafa <serdarmustafa@Serdar-Personal-MBP.local>
2020-04-16 17:26:00 +0300

    set up product overview screen and product item

--- src/screens/shop/ProductsOverviewScreen.js
+++ src/screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import { Flatlist } from 'react-native';
+import React from "react";
+import { Flatlist, Text } from "react-native";
+import { useSelector } from "react-redux";
 
-const ProductsOverviewScreen = () => (
-    
-);
+import ProductItem from "components/shop/ProductItem";
+
+const ProductsOverviewScreen = (props) => {
+  const products = useSelector((state) => state.products.availableProducts);
+  return (
+    <Flatlist
+      data={products}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
+    />
+  );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: "All Products",
+};
 
 export default ProductsOverviewScreen;
